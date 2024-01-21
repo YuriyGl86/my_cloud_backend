759cebc249445893068d7da3783d173e672c299b
Yuriy Glushchenko <yuriygl86@gmail.com>
2024-01-21 19:37:48 +0300

    README, gunicorn

--- my_cloud/my_cloud/settings.py
+++ my_cloud/my_cloud/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / os.getenv('MEDIA_ROOT')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, os.getenv('STATIC_ROOT'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
